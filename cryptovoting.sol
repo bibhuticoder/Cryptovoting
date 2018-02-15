eb750555af931891fcae20e90c32d136822d629d
Bibhuti Poudyal <bibhutipd@gmail.com>
2018-02-15 18:00:03 +0545

    Update cryptovoting.sol

--- cryptovoting.sol
+++ cryptovoting.sol
@@ -2,10 +2,8 @@ pragma solidity ^0.4.0;
 contract CryptoVoting {
     
     struct Candidate{
-        string name;
         string party;
         uint voteCount;
-        bool doesExist;
     }
     
     struct Voter{
@@ -24,27 +22,56 @@ contract CryptoVoting {
     function CryptoVoting() public{
         admin = msg.sender;
         totalVotes = 0;
+        
+        //add 9 candidates initially
+        candidates[1] = Candidate("amalay", 0);
+        candidates[2] = Candidate("congress", 0);
+        candidates[3] = Candidate("maoist", 0);
+        candidates[4] = Candidate("janata", 0);
+        candidates[5] = Candidate("nayashakti", 0);
+        candidates[6] = Candidate("prajatantra", 0);
+        candidates[7] = Candidate("bibeksheel", 0);
+        candidates[8] = Candidate("peasent", 0);
+        candidates[9] = Candidate("nepali", 0);
+        
+        candidateIndex.push(1);
+        candidateIndex.push(2);
+        candidateIndex.push(3);
+        candidateIndex.push(4);
+        candidateIndex.push(5);
+        candidateIndex.push(6);
+        candidateIndex.push(7);
+        candidateIndex.push(8);
+        candidateIndex.push(9);
+        
+        //add 2 voters initially
+        voters[1212] = Voter(false, true);
+        voters[3434] = Voter(false, true);
+        voterIndex.push(1212);
+        voterIndex.push(3434);
+        
+        //set getEndTime
+        electionEnd = (block.timestamp + 24*60*60)*1000;
     }
     
-    modifier onlyAdmin {
-        require(msg.sender == admin);
-        _;
-    }
+    // modifier onlyAdmin {
+    //     require(msg.sender == admin);
+    //     _;
+    // }
     
     modifier isAuthenticatedVoter(uint voterId) {
         require(voters[voterId].hasVoted == false);
         _;
     }
     
-    function addCandidate(uint256 id, string name, string party) public {
-        candidates[id] = Candidate(name, party, 0, true);
+    function addCandidate(uint256 id, string party) public {
+        candidates[id] = Candidate(party, 0);
         candidateIndex.push(id);
     }
     
-    function addVoter(uint _id) public onlyAdmin constant returns(uint){
-        voters[_id] = Voter(false, true);
-        voterIndex.push(_id);
-        return 1;
+    function addVoter(uint256 id) public{
+        voters[id] = Voter(false, true);
+        voterIndex.push(id);
     }
     
     function authenticateVoter(uint voterId)public constant returns(uint){
@@ -65,8 +92,8 @@ contract CryptoVoting {
     }
 
     //get particular candidate
-    function getCandidate(uint256 candidateId) public constant returns(Candidate){
-        return candidates[candidateId];
+    function getCandidate(uint256 candidateId) public constant returns(string, uint){
+        return (candidates[candidateId].party, candidates[candidateId].voteCount);
     }
     
     //get total votes casted(of all Candidates)
@@ -84,18 +111,6 @@ contract CryptoVoting {
     }
     
     event Voted(uint256 total);
-    
-    // event MsgEvent(string message);
-    // string public message;
-    
-    // // // function CryptoVoting(){
-    // // //     message = "initial message";
-    // // // }
-    
-    // function setMessage(string msg)public{
-    //     message = msg;
-    //     MsgEvent(msg);
-    // }
     
     
 }
